c164cc2d76eda4579b573fabf1c787eadfc62dbb
sachinsharma0237 <sachinsharma0237@gmail.com>
2021-05-19 10:40:57 -0700

    navbar completed

--- frontend/portfolio/src/App.js
+++ frontend/portfolio/src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Navbar from './Components/Navbar/Navbar.js';
 
 function App() {
   return (
     <div className="App">
-      Portfolio....
+        <Navbar/>
       </div>
   );
 }
